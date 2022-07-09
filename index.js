5f39d3c8b47e8ada7bf8a05bb36fb23b377a1500
Elizabeth Witengier <ewitengier@ufl.edu>
2022-07-09 11:02:27 -0400

    round 2

--- index.js
+++ index.js
@@ -7,23 +7,56 @@ var weatherSection = document.getElementById('weather')
 var form = document.querySelector('form')
 
 
-
-
 form.onsubmit = function(e) {
   e.preventDefault()
-  var userQuery = this.search.value
+  var userQuery = this.search.value.trim()
+  if (!userQuery) return
+  form.search.value = ""
   fetch(fetchURL)
   .then(function(res) {
+    if (res.status !== 200) {
+      throw new Error ("Location not found")
+    }
     return res.json()
   })
-  .then(function(data) {
-    console.log(data)
+  .then(renderWeather) 
+  .catch(function(err) {
+    weatherSection.innerHTML = err.message
   })
 }
-  
+    
+  function renderWeather(data) {
+    weatherSection.innerHTML = ""
+    var h2 = document.createElement('h2')
+    h2.textContent = data.city + ", " + data.city.country
+    weatherSection.appendChild(h2)
 
-var date = new Date(/* milliseconds go here */)
-var timeString = date.toLocaleTimeString('en-US', {
-  hour: 'numeric',
-  minute: '2-digit'
-})
+    var map = document.createElement('a')
+    map.href = "tel:" + data.coord /* is coord correct? how am I using the long and lat?*/
+    map.textContent = "Click to view map"
+    weatherSection.appendChild(map)
+
+    var icon = document.createElement('img')
+    icon.src = /* dt and url or weather.icon??? */
+    weatherSection.appendChild(icon)
+
+    var description = document.createElement('p')
+    description.textContent = data.weather.description.toUpperCase()
+    weatherSection.appendChild(description)
+
+    weatherSection.appendChild(document.createElement('br'))
+
+    var currentTemp = document.createElement('p')
+    currentTemp.textContent = 'Current:' + convertToF(data.main.temp) + 'F'
+    weatherSection.appendChild(currentTemp)   
+
+    var feelsLike = document.createElement('p')
+    feelsLike.textContent = 'Feels like:' + convertToF(data.main.feels_like) + 'F'
+    weatherSection.appendChild(feelsLike)
+
+    var lastUpdated = document.createElement('p')
+  }
+  
+function convertToF(kelvins) {
+  return Math.round(((kelvins-273.15)*1.8)+32)
+}
